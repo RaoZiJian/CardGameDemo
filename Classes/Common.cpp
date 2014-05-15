4bc5241d0eed6e29aa623023a3e4ca9edb3fc6fb
Kirito <zijian.rao@chukong-inc.com>
2014-05-15 17:00:46 +0800

    Common classes

--- Classes/Common.cpp
+++ Classes/Common.cpp
@@ -8,3 +8,142 @@
 //
 
 #include "Common.h"
+
+Size VisibleRect::winSize(){
+    
+    if(s_winSize.width == 0){
+        
+        s_winSize = Director::getInstance()->getWinSize();
+    }
+    return s_winSize;
+}
+
+Rect VisibleRect::rect(){
+    
+    if(s_rcVisible.size.width==0){
+        
+        auto s = Director::getInstance()->getWinSize();
+        s_rcVisible = Rect(0,0,s.width,s.height);
+    }
+    return s_rcVisible;
+}
+
+Point VisibleRect::center(){
+    
+    if(s_ptCenter.x == 0){
+        
+        auto rc = VisibleRect::rect();
+        s_ptCenter.x = rc.origin.x + rc.size.width/2;
+        s_ptCenter.y = rc.origin.y + rc.size.height/2;
+    }
+    
+    return s_ptCenter;
+}
+
+Point VisibleRect::top(){
+    
+    if (s_ptTop.x==0) {
+        
+        auto rc =VisibleRect::rect();
+        s_ptTop.x = rc.origin.x +rc.size.width/2;
+        s_ptTop.y = rc.origin.y + rc.size.height;
+    }
+    
+    return s_ptTop;
+}
+
+Point VisibleRect::topRight(){
+    
+    if (s_ptTopRight.x==0) {
+        
+        auto rc =VisibleRect::rect();
+        s_ptTopRight.x = rc.origin.x +rc.size.width;
+        s_ptTopRight.y = rc.origin.y + rc.size.height;
+    }
+    
+    return s_ptTopRight;
+}
+
+Point VisibleRect::right(){
+    
+    if (s_ptRight.x==0) {
+        
+        auto rc =VisibleRect::rect();
+        s_ptRight.x = rc.origin.x +rc.size.width;
+        s_ptRight.y = rc.origin.y + rc.size.height/2;
+    }
+    
+    return s_ptRight;
+}
+
+Point VisibleRect::buttomRight(){
+    
+    if (s_ptBottomRight.x==0) {
+        
+        auto rc =VisibleRect::rect();
+        s_ptBottomRight.x = rc.origin.x +rc.size.width;
+        s_ptBottomRight.y = rc.origin.y;
+    }
+    
+    return s_ptBottomRight;
+}
+
+Point VisibleRect::buttom(){
+    
+    if (s_ptBottom.x==0) {
+        
+        auto rc =VisibleRect::rect();
+        s_ptBottom.x = rc.origin.x +rc.size.width/2;
+        s_ptBottom.y = rc.origin.y;
+    }
+    
+    return s_ptBottom;
+}
+
+Rect VisibleRect::buttomLeft(){
+    
+    return VisibleRect::rect();
+}
+
+Point VisibleRect::left(){
+    
+    if (s_ptLeft.x==0) {
+        
+        auto rc =VisibleRect::rect();
+        s_ptLeft.x = rc.origin.x;
+        s_ptLeft.y = rc.origin.y + rc.size.height/2;
+    }
+    
+    return s_ptLeft;
+}
+
+Point VisibleRect::topLeft(){
+    
+    if (s_ptTopLeft.x==0) {
+        
+        auto rc =VisibleRect::rect();
+        s_ptTopLeft.x = rc.origin.x;
+        s_ptTopLeft.y = rc.origin.y + rc.size.height;
+    }
+    
+    return s_ptTopLeft;
+}
+
+bool LoadingMode::init(){
+    
+    if(Layer::init()){
+        
+        auto loadingBg = Sprite::create("IMG_Loading/loading_bg.png");
+        loadingBg->setPosition(Point(VisibleRect::center()));
+        addChild(loadingBg);
+        
+        auto loading = Sprite::create("IMG_Loading/loading.png");
+        loading->setPosition(VisibleRect::center());
+        addChild(loading);
+        
+        auto action = RotateBy::create(2, 360);
+        loading->runAction(RepeatForever::create(action));
+    }
+    
+    return true;
+}
--- Classes/Common.h
+++ Classes/Common.h
@@ -8,11 +8,49 @@
 
 #ifndef CardGameDemo_Common_h
 #define CardGameDemo_Common_h
+#include "cocos2d.h"
 
-typedef struct tagButton{
-    
-    
-}IMG_Button;
+USING_NS_CC;
 
+static Size s_winSize = Size(0,0);
+static Rect s_rcVisible = Rect(0,0,0,0);
+static Point s_ptCenter = Point(0, 0);
+static Point  s_ptTop = Point(0, 0);
+static Point  s_ptTopRight = Point(0, 0);
+static Point  s_ptRight = Point(0, 0);
+static Point  s_ptBottomRight = Point(0, 0);
+static Point  s_ptBottom = Point(0, 0);
+static Point  s_ptLeft = Point(0, 0);
+static Point  s_ptTopLeft = Point(0, 0);
+
+class VisibleRect{
+    
+public:
+    
+    static Size winSize();
+    static Rect rect();
+    static Point center();
+    static Point top();
+    static Point topRight();
+    static Point right();
+    static Point buttomRight();
+    static Rect buttomLeft();
+    static Point buttom();
+    static Point left();
+    static Point topLeft();
+};
+
+//class ModeLayer : public Layer{
+//    
+//
+//};
+
+class LoadingMode : public Layer{
+    
+public:
+    bool init();
+    CREATE_FUNC(LoadingMode);
+    
+};
 
 #endif
